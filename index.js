977d59afbba01084b3e4b25ee694c88eee7961b7
Qwebeck <qwebeck5@gmail.com>
2017-03-29 19:09:48 +0300

    Add files via upload
    Transform response url in number

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ function sum(a,b){
 a+b;
 return
 }
-var numbers = []
+
 
 var server = http.createServer(function(request, response) {
+    console.log(request.url)
+   var number =  request.url.split("/")[1].split("+");
+   var a = number[0];
+   var b = number[1];
+   var x = Number(a);
+   var y = Number(b);
+   console.log(a)
+   console.log(b)
+   console.log(x+y)
     switch (request.url) {
         case '/':
             response.end("Hello");
             break;
             case '/a+b':
-            response.end(sum(9,8));
+            response.end(x+y);
             break;
             default:        
-   response.end("Why it is so hard!?");
+   response.end("Error");
              break;
-            
-
     }
 });
 
